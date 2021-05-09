577aca72a0eea5cee2ba3443ba1b034175335e17
Karshit279 <67937239+Karshit279@users.noreply.github.com>
2021-05-09 22:51:13 +0530

    Changes to be committed: 	modified:   Sector2+.asm 	modified:   compile.bat 	new file:   kernel.bin 	new file:   kernel.c 	new file:   kernel.o 	new file:   ld_script 	new file:   sector2.o

--- Sector2+.asm
+++ Sector2+.asm
@@ -1,17 +1,19 @@
-[org 0x7e00]
 [bits 16]
 
-mov bx, Sector2
-call PrintString
-call StartProtectedMode
+global entry
 
-mov ax,10h
-mov ds,ax
-mov es,ax
-mov fs,ax
-mov gs,ax
-mov ss,ax
-jmp codeseg:EnterProtectedMode
+entry:
+	mov bx, Sector2
+	call PrintString
+	call StartProtectedMode
+
+	mov ax,10h
+	mov ds,ax
+	mov es,ax
+	mov fs,ax
+	mov gs,ax
+	mov ss,ax
+	jmp codeseg:EnterProtectedMode
 
 Sector2:
 	db 'Entering Sector 2+.',0xA,0xD,0
@@ -83,6 +85,86 @@ dataseg equ gdt_datadesc - gdt_nulldesc
 
 [bits 32]
 
+EditGDT:
+	mov [gdt_codedesc + 6], byte 10101111b
+
+	mov [gdt_datadesc + 6], byte 10101111b
+	ret
+
+DetectCPUID:
+
+	pushfd 
+	pop eax
+
+	mov ecx, eax
+
+	xor eax, 1 << 21
+
+	push eax
+	popfd
+
+	pushfd 
+	pop eax
+
+	push ecx 
+	popfd
+
+	xor eax,ecx
+	jz NoCPUID
+	ret
+
+DetectLongMode:
+	mov eax, 0x80000001
+	cpuid
+	test edx, 1 << 29
+	jz NoLongMode
+	ret
+
+NoLongMode:
+	hlt ; No Long Mode Support
+
+NoCPUID:
+
+	hlt ; No CPUID support.
+
+PageTableEntry equ 0x1000
+
+SetUpIdentityPaging:
+
+	mov edi, PageTableEntry
+	mov cr3, edi
+
+	mov dword [edi], 0x2003
+	add edi, 0x1000
+	mov dword [edi], 0x3003
+	add edi, 0x1000
+	mov dword [edi], 0x4003
+	add edi, 0x1000
+
+	mov ebx, 0x00000003
+	mov ecx, 512
+
+	.SetEntry:
+		mov dword [edi], ebx
+		add ebx, 0x1000
+		add edi, 8
+		loop .SetEntry
+
+	mov eax, cr4
+	or eax, 1 << 5
+	mov cr4, eax
+
+	mov ecx, 0xC0000080
+	rdmsr
+	or eax, 1 << 8
+	wrmsr
+
+	mov eax, cr0
+	or eax, 1 << 31
+	mov cr0, eax
+
+	ret
+
 EnterProtectedMode:
 	mov [0xb8000], byte 'P'
 	mov [0xb8002], byte 'R'
@@ -100,6 +182,51 @@ EnterProtectedMode:
 	mov [0xb802a], byte ' '
 	mov [0xb802c], byte '.'
 	mov eax, 1
+	jmp StartLongMode
+
+StartLongMode:
+	call DetectCPUID
+	call DetectLongMode
+	call SetUpIdentityPaging
+	call EditGDT
+	jmp codeseg:Enter64
+
+[bits 64]
+[extern _start]
+
+Enter64:
+
+	call ActivateSSE
+	mov [0xb8000], byte 'L'
+	mov [0xb8002], byte 'O'
+	mov [0xb8004], byte 'N'
+	mov [0xb8006], byte 'G'
+	mov [0xb8008], byte '-'
+	mov [0xb800a], byte 'M'
+	mov [0xb800c], byte 'O'
+	mov [0xb800e], byte 'D'
+	mov [0xb8010], byte 'E'
+	mov [0xb8012], byte ' '
+	mov [0xb8014], byte ' '
+	mov [0xb8016], byte ' '
+	mov [0xb8018], byte ' '
+	mov [0xb802a], byte ' '
+	mov [0xb802c], byte '.'
+	mov rax, 1
+	call _start
+	
 	jmp $
+
+ActivateSSE:
+	mov rax, cr0
+	and ax, 0b11111101
+	or ax, 0b00000001
+	mov cr0, rax
+
+	mov rax, cr4
+	or ax, 0b1100000000
+	mov cr4, rax
+
+	ret
 
 times 16384-($-$$) db 0
