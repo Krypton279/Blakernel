61451d17c2421acf7fc4b0439c5a22acf805069c
Karshit279 <67937239+Karshit279@users.noreply.github.com>
2021-05-10 18:56:18 +0530

    added code to print stuff on the screen with XY implementation

--- kernel.c
+++ kernel.c
@@ -1,17 +1,9 @@
+#include "print.c"
+
 int _start()
 {
-    char* VIDMEM = 0xb8000;
-    VIDMEM[0] = 'K';
-    VIDMEM[2] = 'E';
-    VIDMEM[4] = 'R';
-    VIDMEM[6] = 'N';
-    VIDMEM[8] = 'E';
-    VIDMEM[10] = 'L';
-    VIDMEM[12] = ' ';
-    VIDMEM[14] = ' ';
-    VIDMEM[16] = ' ';
-    VIDMEM[18] = ' ';
-    VIDMEM[20] = ' ';
+	char String[20] = "Kernel Loaded...    ";
+    PrintStringXY(String,20,0,10);
     while(1);
 	return 0;
 }
---
+++ print.c
@@ -0,0 +1,15 @@
+#pragma once
+
+int xyToVideoAddresses(int x,int y,char character) {
+	char *VIDMEM = 0xb8000;
+	VIDMEM[(160 * y) + (x * 2)] = character;
+	// This Expression "(160 * y) + (x * 2)" was simplified by borrrden in Discord , Thanks :D
+	return 0;
+}
+
+int PrintStringXY(char *array,int array_size,int x,int y) {
+	for (int i = 0;i < array_size;i++){
+			xyToVideoAddresses(x+i,y,array[i]);
+	}
+	return 0;
+}
