f0dc7f96ebafe669292fc4fe2781a9b625d9fc97
Karshit279 <67937239+Karshit279@users.noreply.github.com>
2021-05-10 14:12:38 +0530

    Clean Up

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 nasm -f bin boot.asm -o boot.bin
 nasm -f elf64 Sector2+.asm -o sector2.o
-bash -c "gcc -c -ffreestanding -m64 kernel.c -o kernel.o"
-bash -c "ld -nostdlib -T ld_script sector2.o kernel.o -o kernel.bin"
+wsl /usr/local/x86_64elfgcc/bin/x86_64-elf-gcc -c -ffreestanding -m64 kernel.c -o kernel.o
+wsl /usr/local/x86_64elfgcc/bin/x86_64-elf-ld -nostdlib -T ld_script sector2.o kernel.o -o kernel.bin
 copy /b boot.bin+kernel.bin boot.flp
-qemu-system-x86_64 -m 512M boot.flp -no-reboot -no-shutdown
+pause
