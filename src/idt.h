dfc4ff33d43536eeb54fdefc5370e2d9c4a90acb
Karshit279 <67937239+Karshit279@users.noreply.github.com>
2021-05-20 10:37:28 +0530

    Changes to be committed: 	modified:   src/idt.h

--- src/idt.h
+++ src/idt.h
@@ -21,6 +21,22 @@ extern void isr1();
 extern void isr2();
 extern void isr3();
 extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
 
 void InitializeIDT(){
 	// IRQ1
@@ -54,5 +70,133 @@ void InitializeIDT(){
 	IDTEntries[4].type_attr = 15;
 	IDTEntries[4].offset_2 = (uint_16) (isr4 >> 16);
 	IDTEntries[4].offset_3 = (uint_32) (isr4 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ5
+	IDTEntries[4].offset_1 = (uint_16) isr5;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr5 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr5 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ6
+	IDTEntries[4].offset_1 = (uint_16) isr6;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr6 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr6 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ7
+	IDTEntries[4].offset_1 = (uint_16) isr7;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr7 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr7 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ8
+	IDTEntries[4].offset_1 = (uint_16) isr8;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr8 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr8 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ9
+	IDTEntries[4].offset_1 = (uint_16) isr9;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr9 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr9 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ10
+	IDTEntries[4].offset_1 = (uint_16) isr10;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr10 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr10 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ11
+	IDTEntries[4].offset_1 = (uint_16) isr11;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr11 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr11 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ12
+	IDTEntries[4].offset_1 = (uint_16) isr12;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr12 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr12 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ13
+	IDTEntries[4].offset_1 = (uint_16) isr13;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr13 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr13 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ14
+	IDTEntries[4].offset_1 = (uint_16) isr14;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr14 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr14 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ15
+	IDTEntries[4].offset_1 = (uint_16) isr15;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr15 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr15 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ16
+	IDTEntries[4].offset_1 = (uint_16) isr16;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr16 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr16 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ17
+	IDTEntries[4].offset_1 = (uint_16) isr17;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr17 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr17 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ18
+	IDTEntries[4].offset_1 = (uint_16) isr18;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr18 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr18 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ19
+	IDTEntries[4].offset_1 = (uint_16) isr19;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr19 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr19 >> 32);
+	IDTEntries[4].zero = "\0";
+	// IRQ20
+	IDTEntries[4].offset_1 = (uint_16) isr20;
+	IDTEntries[4].selector = 8;
+	IDTEntries[4].ist = "\0";
+	IDTEntries[4].type_attr = 15;
+	IDTEntries[4].offset_2 = (uint_16) (isr20 >> 16);
+	IDTEntries[4].offset_3 = (uint_32) (isr20 >> 32);
 	IDTEntries[4].zero = "\0";
 }
