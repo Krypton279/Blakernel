6f38c547a0bf00dbb90d934e0596d29976f8ea9d
Karshit279 <67937239+Karshit279@users.noreply.github.com>
2021-05-21 21:50:04 +0530

    Changes to be committed: 	modified:   img/boot.flp 	modified:   src/keyboard.c

--- src/keyboard.c
+++ src/keyboard.c
@@ -3,7 +3,7 @@
 #include "print.c"
 
 void keyboard_isr() {
-	PrintStringXY("XV",2,0,14);
+	PrintStringXY("KEY_PRESSED",11,0,14);
 	outb(PIC2_COMMAND,PIC_EOI);
 	outb(PIC1_COMMAND,PIC_EOI);
 	return;
