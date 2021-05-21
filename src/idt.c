5941adf050cc5d4253456bc6888cd59c2c8d97a1
Karshit279 <67937239+Karshit279@users.noreply.github.com>
2021-05-21 13:27:47 +0530

    Changes to be committed: 	modified:   src/idt.c

--- src/idt.c
+++ src/idt.c
@@ -184,7 +184,9 @@ void InitializeIDT(){
 	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
 	outb(PIC1_DATA, ICW4_8086);
 	outb(PIC2_DATA, ICW4_8086);
-	outb(PIC1_DATA, a1);   // restore saved masks.
+	outb(PIC1_DATA, 32);                 // ICW2: Master PIC vector offset
+	outb(PIC2_DATA, 40);                 // ICW2: Slave PIC vector offset
+	outb(PIC1_DATA, a1);  				 // restore saved masks.
 	outb(PIC2_DATA, a2);
 	outb(0x21,0xfd);
 	outb(0xa1,0xff);
