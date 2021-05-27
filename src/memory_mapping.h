fcaa85112cd4191bb39768761bd9977959fa2cfd
Karshit279 <67937239+Karshit279@users.noreply.github.com>
2021-05-27 14:52:47 +0530

    Added Some More Functionality

--- src/memory_mapping.h
+++ src/memory_mapping.h
@@ -10,15 +10,20 @@ struct MemoryRegionEntry {
 };
 
 int ValidMemoryIndex[10];
-
 struct MemoryRegionEntry* MemRegs = (struct MemoryRegionEntry *) 0x500;
+struct MemoryRegionEntry heapMemory;
 
 void ValidMemRegs() { 
 	int j = 0;
 	for(int i = 0;i < MemRegCounts;i++){
 		if(MemRegs[i].MemoryType == 1){
 			ValidMemoryIndex[j] = i;
+			j++;
 		}
 	}
+	heapMemory.Address = MemRegs[ValidMemoryIndex[1]].Address;
+	heapMemory.Length = MemRegs[ValidMemoryIndex[1]].Length;
+	heapMemory.MemoryType = MemRegs[ValidMemoryIndex[1]].MemoryType;
+	heapMemory.Attr_Types = MemRegs[ValidMemoryIndex[1]].Attr_Types;
 	return;
 }
