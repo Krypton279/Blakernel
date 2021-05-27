f185a74d98ad2f43d6f4f2a793bd15721d2bef9a
Karshit279 <67937239+Karshit279@users.noreply.github.com>
2021-05-27 19:58:20 +0530

    pub prob

--- src/memory_mapping.h
+++ src/memory_mapping.h
@@ -9,9 +9,17 @@ struct MemoryRegionEntry {
 	uint_32 Attr_Types;
 };
 
+struct HeapAddress {
+	uint_64 Address;
+	int isFree;
+};
+
 int ValidMemoryIndex[10];
 struct MemoryRegionEntry* MemRegs = (struct MemoryRegionEntry *) 0x500;
 struct MemoryRegionEntry heapMemory;
+uint_64 HeapLengthKB = 510;
+uint_64 DividedHeapAddresses[HeapLengthKB];
+struct HeapAddress HeapAddresses[HeapLengthKB]; 
 
 void ValidMemRegs() { 
 	int j = 0;
@@ -25,5 +33,29 @@ void ValidMemRegs() {
 	heapMemory.Length = MemRegs[ValidMemoryIndex[1]].Length;
 	heapMemory.MemoryType = MemRegs[ValidMemoryIndex[1]].MemoryType;
 	heapMemory.Attr_Types = MemRegs[ValidMemoryIndex[1]].Attr_Types;
+	return;
+}
+
+void DivideMemSectors(){
+	uint_64 OffsetFromFirstDivision[(heapMemory.Length)/1024];
+	uint_64 LastAddress = (heapMemory.Address + heapMemory.Length) - (heapMemory.Length/1024);
+	uint_64 Length = heapMemory.Length;
+	uint_64 Address = heapMemory.Address;
+	for(int i = 0;i < (heapMemory.Length/1024);i++){
+		OffsetFromFirstDivision[i] = i;
+	}
+	for(int j = 0;j < (heapMemory.Length/1024);j++){
+		HeapAddresses[j].Address = heapMemory.Address + OffsetFromFirstDivision[j];
+		HeapAddresses[j].isFree = 0;
+	}
+}
+
+void malloc(uint_64 size){
+	int remainder = size % 8;
+	size = size - remainder;
+	if((size % 8) != 0){
+		size = size - (2*remainder);
+	}
+
 	return;
 }
